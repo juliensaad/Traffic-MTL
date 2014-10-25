815184d9ddccb9df063d13cd14f84857f033f26f
saadjulien <julien.saad@polymtl.ca>
2014-10-25 12:52:05 -0400

    Widget

---
+++ ios/Traffic MTL/Trafic Widget/TodayViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TodayViewController.swift
+//  Trafic Widget
+//
+//  Created by Julien Saad on 2014-07-25.
+//  Copyright (c) 2014 Développements Third Bridge Inc. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController {
+    
+    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        // Custom initialization
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.preferredContentSize = CGSizeMake(320, 100);
+        // Do any additional setup after loading the view from its nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
+        // Perform any setup necessary in order to update the view.
+
+        // If an error is encoutered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+
+        completionHandler(NCUpdateResult.NewData)
+    }
+    
+}
